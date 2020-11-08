18372d6a2f903108e1db30ca75fe1e1293756712
Robert Radnai <robert.radnai@gmail.com>
2020-11-08 12:48:42 +0100

    Added exception handling for loading from shelve

--- vocabulary_mgr/storage.py
+++ vocabulary_mgr/storage.py
@@ -1,4 +1,6 @@
 from shelve import DbfilenameShelf, open
+from time import sleep
+from traceback import format_exception
 
 
 class StorageManager:
@@ -6,8 +8,16 @@ class StorageManager:
         self._storage_path = storage_path
 
     def get_item(self, element_id: str):
-        with open(self._storage_path) as storage:
-            return storage[element_id]
+        attempts = 1
+        while attempts <= 5:
+            try:
+                with open(self._storage_path) as storage:
+                    return storage[element_id]
+            except Exception as e:
+                print(f"WARNING - Exception occurred during accessing the shelf object, attempt {attempts}. {format_exception(e)}")
+                sleep(1)
+        raise Exception("Shelve object couldn't be accessed after several attempts in get_item!")
+
 
     def put_item(self, element_id: str, item_to_store: object):
         with open(self._storage_path) as storage:
