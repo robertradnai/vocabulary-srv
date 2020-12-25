19b8397d3faa738b291d88f3c4ac8d258baf28a7
Robert Radnai <robert.radnai@gmail.com>
2020-12-25 18:28:10 +0100

    Revert "Updated version information route"
    This reverts commit 8d582aedeedb162e841f11b46d7b2901530cf135.

--- vocabulary_srv/__init__.py
+++ vocabulary_srv/__init__.py
@@ -109,7 +109,7 @@ def create_app(test_config=None):
     app.register_blueprint(vocabulary.bp)
 
 
-    @app.route("/api/hello")
+    @app.route("/hello")
     def hello():
         return jsonify({"build": "2020-12-25 test"})
 
