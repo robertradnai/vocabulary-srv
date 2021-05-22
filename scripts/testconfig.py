dec529c4c175a7d7c1fbc223f98b293fe764e2b5
Robert Radnai <robert.radnai@gmail.com>
2021-05-22 12:37:53 +0200

    Fixed SQLAlchemy breaking change

--- scripts/testconfig.py
+++ scripts/testconfig.py
@@ -3,6 +3,6 @@
 SHARED_WORKBOOKS_PATH = "testdata/shared_collections"
 SHARED_WORKBOOKS_METADATA = "testdata/shared_collections_metadata.yml"
 
-SQLALCHEMY_DATABASE_URI = "postgres://vocabulary_test:vocabulary_test@localhost/vocabulary_test"
+SQLALCHEMY_DATABASE_URI = "postgresql://vocabulary_test:vocabulary_test@localhost/vocabulary_test"
 
 SECRET_KEY="NzdkYTNmZGMxNmY2NjNhNDRjYzc4NTU2NTFkNDBiMGFkZWQzYjcwOTc0OGUxMTMy"
