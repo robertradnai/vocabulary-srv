f74e17cde08fbe8c23a94f1d575e6c85a7f92546
Robert Radnai <robert.radnai@gmail.com>
2021-08-10 11:22:41 +0200

    DB container deletes itself

--- scripts/docker_db_run.sh
+++ scripts/docker_db_run.sh
@@ -1,4 +1,4 @@
-sudo docker run --name postgres-test -d -p 5432:5432 -e POSTGRES_PASSWORD=vocabulary_test -e POSTGRES_USER=vocabulary_test postgres
+sudo docker run --rm --name postgres-test -d -p 5432:5432 -e POSTGRES_PASSWORD=vocabulary_test -e POSTGRES_USER=vocabulary_test postgres
 
 # Look into the container with sudo docker exec -it postgres-test /bin/bash
 # Wait half a minute between starting db Docker image and launching the app
