348c8841da02a898de6fe887399c39e0653ff843
Robert Radnai <robert.radnai@gmail.com>
2021-08-10 15:40:12 +0200

    Made logging configurable and refactoring

--- vocabulary_srv/__init__.py
+++ vocabulary_srv/__init__.py
@@ -1,34 +1,13 @@
 import os
-from logging.config import dictConfig
-
+from logging.config import dictConfig as loggingDictConfig
 from flask import Flask, jsonify, g
-
 from vocabulary_srv.dataaccess import IWordCollectionsDao
 from .database import db
 from .database import init_db, init_app, DbWordListStorage
 
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
-
-security = None
-
 
 def create_app(test_config=None, config_filename=None):
     """Create and configure an instance of the Flask application."""
-
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -42,12 +21,13 @@ def create_app(test_config=None, config_filename=None):
     # In order to enable easy tweaking of app configuration
     # From the command line
     if config_filename is not None:
-        config_path=os.path.join(app.instance_path, config_filename)
+        config_path = os.path.join(app.instance_path, config_filename)
         if os.path.isfile(config_path):
             app.config.from_pyfile(config_path)
             app.logger.debug(f"Loaded configuration from {config_path}.")
         else:
-            raise FileNotFoundError(f"Configuration file at {config_path} is missing or access isn't granted, terminating...")
+            raise FileNotFoundError(f"Configuration file at {config_path} "
+                                    f"is missing or access isn't granted, terminating...")
     else:
         app.logger.debug(f"No configuration file was provided.")
 
@@ -58,10 +38,10 @@ def create_app(test_config=None, config_filename=None):
     # Check if connection string is defined
     if app.config.get("SQLALCHEMY_DATABASE_URI") is None:
         exc_msg = "SQLALCHEMY_DATABASE_URI is not set, terminating app..."
-        app.logger.debug(exc_msg) # Gunicorn doesn't print exception message, so I print it here additionally
+        app.logger.debug(exc_msg)  # Gunicorn doesn't print exception message, so I print it here additionally
         raise ValueError(exc_msg)
 
-    if app.config.get("SECRET_KEY") is None and (app.testing):
+    if app.config.get("SECRET_KEY") is None and app.testing:
         app.config["SECRET_KEY"] = os.urandom(24)
         app.logger.debug("Debug or test mode detected, generating secret key...")
 
@@ -69,6 +49,22 @@ def create_app(test_config=None, config_filename=None):
         exc_msg = "Secret key isn't found in the provided configuration for production app!"
         app.logger.debug(exc_msg)
         raise ValueError(exc_msg)
+
+    loggingDictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }},
+        'handlers': {'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': app.config.get("LOGGING_LEVEL", "INFO"),
+            'handlers': ['wsgi']
+        }
+    })
 
     # Ensure that the instance folder exists
     try:
@@ -92,7 +88,7 @@ def create_app(test_config=None, config_filename=None):
     def hello():
         return jsonify({"build": "2020-12-25 test"})
 
-    init_app(app) # Register database command for flask
+    init_app(app)  # Register database command for flask
 
     return app
 
