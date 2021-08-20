49bb4b78e4a9108406527789b1f055230e93a275
Robert Radnai <robert.radnai@gmail.com>
2021-08-20 23:12:18 +0200

    Deleted obsolete config file (testconfig.py took its place)

--- scripts/dockerconfig.py
+++
@@ -1,8 +0,0 @@
-
-
-SHARED_WORKBOOKS_PATH = "/config/shared_collections"
-SHARED_WORKBOOKS_METADATA = "/config/shared_collections_metadata.yml"
-
-SQLALCHEMY_DATABASE_URI = "postgresql://vocabulary_test:vocabulary_test@localhost/vocabulary_test"
-
-SECRET_KEY="NzdkYTNmZGMxNmY2NjNhNDRjYzc4NTU2NTFkNDBiMGFkZWQzYjcwOTc0OGUxMTMy"
