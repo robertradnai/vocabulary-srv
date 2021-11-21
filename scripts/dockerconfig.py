fd69b5d30034105501f369218612c7db7ed718c6
Robert Radnai <robert.radnai@gmail.com>
2021-11-21 01:10:38 +0100

    Added new configuration due to changed Docker test host

---
+++ scripts/dockerconfig.py
@@ -0,0 +1,3 @@
+SQLALCHEMY_DATABASE_URI = "postgresql://vocabulary_test:vocabulary_test@postgres-test/vocabulary_test"
+SECRET_KEY="NzdkYTNmZGMxNmY2NjNhNDRjYzc4NTU2NTFkNDBiMGFkZWQzYjcwOTc0OGUxMTMy"
+AWS_COGNITO_DOMAIN = 'https://vocabulary-test-local.auth.eu-central-1.amazoncognito.com'  # TODO what is it?
